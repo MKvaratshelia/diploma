a7bf7b872b4240219d76f93bcb9c16b2e2474657
Kvaratshelia Mihail <kvaratsheliamihail@yandex.ru>
2020-03-17 11:54:07 +0300

    first commit

---
+++ src/js/components/slider.js
@@ -0,0 +1,24 @@
+import "../../vendor/glide/glide.core.css";
+import "../../vendor/glide/glide.theme.css";
+import Glide from "@glidejs/glide";
+
+let slider = new Glide(".glide", {
+  type: "slider",
+  focusAt: "0",
+  perView: 3,
+  peek: { before: 88, after: 88 },
+  gap: 16,
+  breakpoints: {
+    768: {
+      peek: { before: 40, after: 40 },
+      perView: 2,
+      gap: 8
+    },
+    425: {
+      perView: 1,
+      peek: { before: 0, after: 0 }
+    }
+  }
+}).mount();
+
+export { slider };
